b47e76fb000eccd267aa3fbdf1d0d9a258d017f5
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-07-27 16:07:45 -0400

    need help with seasonal

--- client/src/screens/Seasonal.jsx
+++ client/src/screens/Seasonal.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
+import '../assets/css/Seasonal.css'
 
 export default function Seasonal(props) {
   const { seasonsList } = props;
+  const [seasonId, setSeasonId] = useState()
+
+  
 
   return (
     <div  className='screen-width-div'>
-      <p className='p'>Seasonal</p>
+      <p className='b-text'>View Produce by Seasons</p>
+      <br/>
+      <select
+          onChange={(e)=>setSeasonId(e.target.value)}
+          defaultValue='default'
+          className='seasonal-selector'
+          name='season'>
+          {/* value={season.name} */}
+					<option disabled value='default'>
+						- Select a Season -
+					</option>
+          {seasonsList.map((season, idx) => (
+            <option key={idx} value={season.id}>{season.name}</option>
+          ))}
+          </select>
 
 
     </div>
